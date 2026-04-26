eb829a8c7cb6e8898568e2e0d4d290e0da56408e
revanth <revanthrao64@gmail.com>
2026-04-26 23:23:30 +0530

    removed node modules

--- frontend/dist/assets/dateUtils-dsJM_BFw.js
+++
@@ -1,1 +0,0 @@
-function n(t=new Date){const e=t.getTimezoneOffset();return new Date(t.getTime()-e*60*1e3).toISOString().split("T")[0]}function a(t=new Date){return n(t).slice(0,7)}export{n as a,a as g};
